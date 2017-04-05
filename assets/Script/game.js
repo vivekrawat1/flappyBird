c3162351f7f8b3d88ca1d7471eea02c565e4dcb7
vivekRawat <vivek.rawat49@gmail.com>
2017-04-06 00:21:36 +0530

    some changes

--- assets/Script/game.js
+++ assets/Script/game.js
@@ -2,23 +2,27 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //    default: null,      // The default value will be used only when the component attaching
-        //                           to a node for the first time
-        //    url: cc.Texture2D,  // optional, default is typeof default
-        //    serializable: true, // optional, default is true
-        //    visible: true,      // optional, default is true
-        //    displayName: 'Foo', // optional
-        //    readonly: false,    // optional, default is false
-        // },
-        // ...
+        touchNode: {
+            default: null,
+            type: cc.Node
+        },
     },
 
     // use this for initialization
     onLoad: function () {
-
+        
+        this.test();
     },
-
+  
+    test: function () {
+        cc.eventManager.addListener({
+            event: cc.EventListener.MOUSE,
+            onMouseDown: function (keyCode, event) { console.log("don't let me down") },
+            onMouseUp: function (keyCode, event) { console.log('high all the time') },
+            onMouseMove: function () { this.touchNode.setPosition = cc.Event.EventMouse.getLocation()},
+            onMouseScroll: function () { console.log("checck yar") },
+        }, this.node);
+    }
     // called every frame, uncomment this function to activate update callback
     // update: function (dt) {
 
